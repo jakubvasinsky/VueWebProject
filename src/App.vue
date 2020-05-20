049abb7328f919b7d10d43db0d41a13b56d1c4fd
jakubvasinsky <55527876+jakubvasinsky@users.noreply.github.com>
2020-05-20 16:59:53 +0200

    In progress

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,34 @@
 <template>
   <div id="app">
-    <header class="header">{{text1}}</header>
-    <h2>{{text2}}</h2>
-    <div class="text3">
-      Our Artificial Intelligence powered tools use millions of project data points
-      to ensure that your project is successful
+    <header id="header">
+      {{text1}}
+      <div id="bold-text">
+        <b>{{text2}}</b>
+      </div>
+    </header>
+    <div id="text3">Our Artificial Intelligence powered tools use millions of project data
+      <div>
+        points
+        to ensure that your project is successful
+      </div>
+    </div>
+
+    <div id="box1">
+      <h3>Supervisor</h3>Monitors activity to identify project roadblocks
+    </div>
+
+    <div id="box2">
+      <h3>Team Builder</h3>Scans our talent network to
+      create the optimal team for your project
+    </div>
+
+    <div id="box3">
+      <h3>Karma</h3>Regularly evaluates our talent to ensure quality
+    </div>
+
+    <div id="box4">
+      <h3>Calculator</h3>Uses data from past projects
+      to provide better delivery estimates
     </div>
   </div>
 </template>
@@ -29,6 +53,20 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 30px;
+}
+#header {
+  font-size: 150%;
+}
+#text3 {
+  font-size: 80%;
+  margin: 20px;
+  color: gray;
+}
+#box1 {
+  border: 5px, color black;
+  width: 350px;
+  height: 200px;
+  color: gray;
 }
 </style>
